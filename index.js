71bab597ecc1d47e664730428843d781ad86969a
David Moina <davemolina777@gmail.com>
2021-05-25 13:04:22 -0500

    commit tree 61b39c59359454cb5f813b11e3273b507ffb77f0

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map (sourceArray, callbackFunction) {
+    let array = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        array.push(callbackFunction(sourceArray[i]))
+    }
+return array
+}
+function reduce (sourceArray, callbackFunction, startingValue) {
+    let total;
+    if (startingValue) {
+        total = startingValue;
+        for (let i = 0; i < sourceArray.length; i++) {
+            total = callbackFunction(sourceArray[i], total)
+        }
+        return total 
+    } else {
+        total = sourceArray[0];
+        for (let i = 1; i < sourceArray.length; i++) {
+            total = callbackFunction(sourceArray[i], total)
+    }
+    return total
+    }
+}
